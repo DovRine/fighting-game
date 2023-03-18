b89d2ec12ab1546a804ebde714531f20ed6df471
Dov Rine <dov.rine@gmail.com>
2023-03-17 20:13:19 -0700

    refactor(Fighter): simplify constructor

--- src/js/Fighter/Fighter.js
+++ src/js/Fighter/Fighter.js
@@ -1,4 +1,4 @@
-import { setCenterPosition } from '../utils/index.js'
+import { setCenterPosition, setPositions } from '../utils/index.js'
 import { draw } from './draw.js'
 import { update } from './update.js'
 
@@ -14,14 +14,10 @@ class Fighter {
             x: 0,
             y: 0
         }
-        this.width = 100
-        this.height = 100
+        this.width = 50
+        this.height = 150
         this.color = color
-        this.top = this.position.y
-        this.right = this.position.x + this.width
-        this.left =
-        this.bottom = this.position.x + this.height
-        this.setCenterPosition()
+        setPositions.call(this)
     }
     draw = draw.bind(this)
     setCenterPosition = setCenterPosition.bind(this)
