fdae610ee1da0a913e4c940207a2b82305595aee
Dov Rine <dov.rine@gmail.com>
2023-03-17 20:14:07 -0700

    fix(Game::animate): deltaTime calculated backward

--- src/js/Game/animate.js
+++ src/js/Game/animate.js
@@ -1,12 +1,12 @@
 
 function animate(timestamp) {
     window.requestAnimationFrame(this.animate.bind(this))
-    // const deltaTime = this.lastTime - timestamp
+    const deltaTime = timestamp - this.lastTime
     this.lastTime = timestamp
     this.clearScreen()
 
     for (const fighter of this.fighters) {
-        fighter.update()
+        fighter.update(deltaTime)
     }
 }
 
