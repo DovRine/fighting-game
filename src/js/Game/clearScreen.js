cf31fdac3766c03b416efc298e33c300e286112b
Dov Rine <dov.rine@gmail.com>
2023-03-17 20:14:28 -0700

    fix(Game::clearScreen): wrong canvas method

--- src/js/Game/clearScreen.js
+++ src/js/Game/clearScreen.js
@@ -1,6 +1,6 @@
 function clearScreen() {
     this.ctx.fillStyle = 'black'
-    this.ctx.fillRect(0, 0, this.width, this.height)
+    this.ctx.clearRect(0, 0, this.width, this.height)
 }
 
 export { clearScreen }
